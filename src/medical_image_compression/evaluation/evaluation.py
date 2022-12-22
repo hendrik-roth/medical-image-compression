96f2ed95d4d88f809319603e22da63de48f88219
hendrik-roth <hendrik@die5roths.de>
2022-12-22 12:15:06 +0100

    :ambulance: fixed fsim calculation

--- src/medical_image_compression/evaluation/evaluation.py
+++ src/medical_image_compression/evaluation/evaluation.py
@@ -15,7 +15,8 @@ class Evaluator:
     def __init__(self,
                  images,
                  compression_method,
-                 out_path):
+                 out_path,
+                 decompression_method=None):
         """init evaluator
 
         Args:
@@ -27,6 +28,7 @@ class Evaluator:
         self.compression_method = compression_method
         self.out_path = out_path
         self.create_dir_safely(self.out_path)
+        self.decompression_method = decompression_method
 
     def evaluate(self):
         """main function to perform complete evaluation
@@ -56,18 +58,21 @@ class Evaluator:
                 self.compression_method(image)
                 t1 = time()
                 compression_time = t1 - t0
-                # save compressed file
-                compressed_path = f"{self.out_path}/{idx}/compressed-{datetime.datetime.now():%Y-%m-%d-%H-%M-%S-%f}.dcm"
+                # if intended to decompress
+                if self.decompression_method is not None:
+                    self.decompression_method(image)
+                # save new image
+                new_image_path = f"{self.out_path}/{idx}/reconstructed-{datetime.datetime.now():%Y-%m-%d-%H-%M-%S-%f}.dcm"
                 self.create_dir_safely(f"{self.out_path}/{idx}/")
-                image.save_as(compressed_path)
+                image.save_as(new_image_path)
 
                 # Medical Image Quality Assessment (MIQA)
-                miqa = Miqa(image, dcmread(compressed_path))
+                miqa = Miqa(image, dcmread(new_image_path), filename, new_image_path)
                 ssim, fsim = miqa.get_miqa_metrics()
 
                 # Compression Assessment
                 cr = Path(f"{filename}").stat().st_size / Path(
-                    compressed_path).stat().st_size
+                    new_image_path).stat().st_size
 
                 # add to lists
                 t_list.append(compression_time)
--- src/medical_image_compression/miqa/miqa.py
+++ src/medical_image_compression/miqa/miqa.py
@@ -1,13 +1,22 @@
 import numpy as np
+import torch
+import cv2
+import piq
+
+from pydicom import dcmread
+from skimage.io import imread
+from skimage import exposure, img_as_ubyte
 
 from skimage.metrics import structural_similarity
-from image_similarity_measures.quality_metrics import fsim
 
 
 class Miqa:
-    def __init__(self, reference_image, compressed_image):
+    def __init__(self, reference_image, compressed_image, path_reference_img, path_compressed_img):
+        self.fsim = 0
         self.reference_image = reference_image
         self.compressed_image = compressed_image
+        self.path_reference = path_reference_img
+        self.path_compressed = path_compressed_img
 
     def get_miqa_metrics(self):
         """calculating ssim and fsim
@@ -25,8 +34,28 @@ class Miqa:
                                       sigma=1.5,
                                       use_sample_covariance=False,
                                       data_range=data_range)
+        self.calc_fsim()
+        return _ssim, self.fsim
 
-        # _fsim = fsim(self.reference_image.pixel_array,
-        #             self.compressed_image.pixel_array)
+    @torch.no_grad()
+    def calc_fsim(self):
+        # transform greyscale into rgb channels
+        reference = imread(self.path_reference)
+        reference_8bit = img_as_ubyte(exposure.rescale_intensity(reference))
+        reference_rgb_channels = cv2.cvtColor(reference_8bit, cv2.COLOR_GRAY2RGB)
 
-        return _ssim, 1
+        compressed = dcmread(self.path_compressed).pixel_array
+        compressed_8bit = img_as_ubyte(exposure.rescale_intensity(compressed))
+        compressed_rgb_channels = cv2.cvtColor(compressed_8bit, cv2.COLOR_GRAY2RGB)
+
+        # create image tensors
+        x = torch.tensor(reference_rgb_channels).permute(2, 0, 1)[None, ...] / 255.
+        y = torch.tensor(compressed_rgb_channels).permute(2, 0, 1)[None, ...] / 255.
+
+        if torch.cuda.is_available():
+            # Move to GPU to make computaions faster
+            x = x.cuda()
+            y = y.cuda()
+
+        fsim_index: torch.Tensor = piq.fsim(x, y, data_range=255, reduction='none', chromatic=False)
+        self.fsim = float(f"{fsim_index.item():0.4f}")
